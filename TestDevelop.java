49795cb31688acecb42a9a6640c67b04ec64b96d
ganamadu <ganapathi.madu@gmail.com>
2024-03-21 17:05:21 +0530

    m1 method added

--- TestDevelop.java
+++ TestDevelop.java
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
 public void test() {
 	System.out.println("This is test method()...");
+=======
+public void m1() {
+	System.out.println("This is m1 method....");
+>>>>>>> Stashed changes
 }
